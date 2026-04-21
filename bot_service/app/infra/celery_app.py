42356bec4a7b9770709eee99b7568f65de164f13
Elizaveta Konovalova <Eliza050203@gmail.com>
2026-04-21 16:08:02 +0300

    feat(bot): add redis, celery clients

---
+++ bot_service/app/infra/celery_app.py
@@ -0,0 +1,10 @@
+from celery import Celery
+from app.core.config import settings
+
+celery_app = Celery(
+    "bot_service",
+    broker=settings.rabbitmq_url,
+    backend=settings.redis_url
+)
+
+celery_app.autodiscover_tasks(["app.tasks"])
---
+++ bot_service/app/infra/redis.py
@@ -0,0 +1,11 @@
+from redis.asyncio import Redis
+from functools import lru_cache
+
+from app.core.config import settings
+
+@lru_cache
+def get_redis():
+    return Redis.from_url(
+        settings.redis_url,
+        decode_responses=True
+    )
