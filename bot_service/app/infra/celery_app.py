845653d034b2d844e3191333a420f552719dd5f6
Elizaveta Konovalova <Eliza050203@gmail.com>
2026-04-28 14:02:28 +0300

    fix(bot): infra layer: add sync redis and type-fixing

--- bot_service/app/infra/celery_app.py
+++ bot_service/app/infra/celery_app.py
@@ -3,8 +3,10 @@ from app.core.config import settings
 
 celery_app = Celery(
     "bot_service",
-    broker=settings.rabbitmq_url,
-    backend=settings.redis_url
+    broker=str(settings.rabbitmq_url),
+    backend=str(settings.redis_url)
 )
 
 celery_app.autodiscover_tasks(["app.tasks"])
+
+import app.tasks.llm_tasks # noqa: E402,E401
--- bot_service/app/infra/redis.py
+++ bot_service/app/infra/redis.py
@@ -1,13 +1,23 @@
-from redis.asyncio import Redis
+from redis.asyncio import Redis as AsyncRedis
+import redis
 from functools import lru_cache
 
 from app.core.config import settings
 
 @lru_cache
 def get_redis():
-    return Redis.from_url(
-        settings.redis_url,
+    return AsyncRedis.from_url(
+        str(settings.redis_url),
+        decode_responses=True
+    )
+
+
+@lru_cache
+def get_sync_redis():
+    return redis.Redis.from_url(
+        str(settings.redis_url),
         decode_responses=True
     )
 
 redis_client = get_redis()
+sync_redis_client = get_sync_redis()
